1a4811953c177019d900e59616c7c82870c61b82
mdsouza5 <mdsouza5@hawk.iit.edu>
2018-02-23 16:04:25 -0600

    TextView & SeekBar Updated

--- app/src/main/java/com/example/mdsouza5/hw5slideshow/MainActivity.java
+++ app/src/main/java/com/example/mdsouza5/hw5slideshow/MainActivity.java
@@ -4,27 +4,57 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button startSlideShowBtn;
-    private SeekBar timerseekBar;
+    private Button slideslideshowButton;
+    private SeekBar timerSeekBar;
+    private TextView timerTextView;
+    private ImageView iitlogoImageView;
+    private int timerForSlideShow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //findViewById for
-        startSlideShowBtn = (Button) findViewById(R.id.startSlideShowMABtn);
-        timerseekBar = (SeekBar) findViewById(R.id.seekbarTimerMA);
-        timerseekBar.setMax(10);
-        timerseekBar.setProgress(timerseekBar.getMax() / 2);
+        //findViewById for all
+        slideslideshowButton = (Button) findViewById(R.id.startSlideShowMABtn);
+        timerSeekBar = (SeekBar) findViewById(R.id.seekbarTimerMA);
+        timerTextView = (TextView) findViewById(R.id.seekbarTimerValueMA);
 
-        startSlideShowBtn.setOnClickListener(new View.OnClickListener() {
+        //Set Values For Controls
+        timerSeekBar.setMax(10);
+        timerSeekBar.setProgress(timerSeekBar.getMax() / 2);
+        timerTextView.setText(String.valueOf(timerSeekBar.getProgress()));
+
+        //SeekBar
+        timerSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int seekbarProgress, boolean b) {
+                timerForSlideShow = Integer.valueOf(seekbarProgress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                timerTextView.setText(String.valueOf(timerSeekBar.getProgress()));
+            }
+        });
+
+        //Button to Call the Next Activity and Start SlideShow
+        slideslideshowButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Slide Show Started", Toast.LENGTH_SHORT).show();
                 //Get the Values
 
                 //Call intent and pass values to the next activity
